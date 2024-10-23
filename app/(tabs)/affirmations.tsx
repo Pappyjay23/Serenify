e662bb35d073652d01fd0d543a5d17ada9e1c6cc
Pappyjay23 <pjinadu02@gmail.com>
2024-10-23 11:23:27 +0100

    feat: Updated styles for affirmations and quotes, also added loader while it fetches data

--- app/(tabs)/affirmations.tsx
+++ app/(tabs)/affirmations.tsx
@@ -4,25 +4,36 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import CustomButton from "@/components/CustomButton";
 
 const Affirmations = () => {
-	const [quote, setQuote] = useState("When a purpose of a thing is not known, abuse is inevitable.");
+	const [quote, setQuote] = useState(
+		"When the purpose of a thing is not known, abuse is inevitable."
+	);
+	const [author, setAuthor] = useState("Albert Einstein.");
+	const [isQuoteLoading, setIsQuoteLoading] = useState(false);
+
 	const [affirmation, setAffirmation] = useState(
 		"You are better than you think."
 	);
-	const [author, setAuthor] = useState("Albert Einstein.");
+	const [isAffirmationLoading, setIsAffirmationLoading] = useState(false);
 
 	const getQuote = async () => {
+		setIsQuoteLoading(true);
 		fetch("https://zenquotes.io/api/random")
 			.then((res) => res.json())
 			.then((data) => {
 				setQuote(data[0].q);
 				setAuthor(data[0].a);
+				setIsQuoteLoading(false);
 			});
 	};
 
 	const getAffirmation = async () => {
+		setIsAffirmationLoading(true);
 		fetch("https://www.affirmations.dev/")
 			.then((res) => res.json())
-			.then((data) => setAffirmation(data.affirmation));
+			.then((data) => {
+				setAffirmation(data.affirmation);
+				setIsAffirmationLoading(false);
+			});
 	};
 
 	useEffect(() => {
@@ -44,9 +55,8 @@ const Affirmations = () => {
 						Choose an affirmation to affirm to yourself.
 					</Text>
 				</View>
-				<ScrollView showsVerticalScrollIndicator={false} className="my-4">
-          {/* Quotes */}
-
+				<ScrollView showsVerticalScrollIndicator={false} className='my-4'>
+					{/* Quotes */}
 					<View className='justify-center items-center mb-5'>
 						<ScrollView showsVerticalScrollIndicator className='w-full'>
 							<View className='bg-[#fff]/20 border-[#fff] border rounded-lg p-4 flex flex-col space-y-3 w-[80%] mx-auto h-full overflow-y-scroll'>
@@ -63,16 +73,17 @@ const Affirmations = () => {
 							onPress={() => {
 								getQuote();
 							}}
-							buttonStyles='w-[80%] mt-4 bg-[#fff]/10'
-							textStyles='font-hmedium'
+							buttonStyles='w-[80%] mt-4 bg-[#fff]'
+							textStyles='font-hmedium text-[#0E4351]'
+							isLoading={isQuoteLoading}
 						/>
 					</View>
 
-          {/* Affirmations */}
+					{/* Affirmations */}
 					<View className='justify-center items-center'>
 						<ScrollView showsVerticalScrollIndicator className='w-full'>
-							<View className='bg-[#fff] rounded-lg p-4 flex flex-col space-y-3 w-[80%] mx-auto h-full overflow-y-scroll'>
-								<Text className='text-4xl font-hlight text-[#0E4351]'>
+							<View className='bg-[#fff]/20 border-[#fff] border rounded-lg p-4 flex flex-col space-y-3 w-[80%] mx-auto h-full overflow-y-scroll'>
+								<Text className='text-4xl font-hlight text-[#fff]'>
 									{affirmation}
 								</Text>
 							</View>
@@ -82,8 +93,9 @@ const Affirmations = () => {
 							onPress={() => {
 								getAffirmation();
 							}}
-							buttonStyles='w-[80%] mt-4 bg-[#fff]/10'
-							textStyles='font-hmedium'
+							buttonStyles='w-[80%] mt-4 bg-[#fff]'
+							textStyles='font-hmedium text-[#0E4351]'
+							isLoading={isAffirmationLoading}
 						/>
 					</View>
 				</ScrollView>
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,16 +1,33 @@
-import { Text, TouchableOpacity } from "react-native";
+import { isLoading } from "expo-font";
+import { ActivityIndicator, Text, TouchableOpacity } from "react-native";
 
 interface CustomButtonProps {
 	title: string;
 	onPress: () => void;
-    buttonStyles?: string;
-    textStyles?: string;
+	buttonStyles?: string;
+	textStyles?: string;
+	isLoading?: boolean;
 }
 
-const CustomButton = ({ onPress, title, buttonStyles, textStyles }: CustomButtonProps) => {
+const CustomButton = ({
+	onPress,
+	title,
+	buttonStyles,
+	textStyles,
+	isLoading = false,
+}: CustomButtonProps) => {
 	return (
-		<TouchableOpacity activeOpacity={0.8} onPress={onPress} className={`w-full p-4 bg-transparent border-white border rounded-lg justify-center items-center ${buttonStyles}`}>
-			<Text className={`text-white font-psemibold ${textStyles}`}>{title}</Text>
+		<TouchableOpacity
+			activeOpacity={0.8}
+			onPress={onPress}
+			className={`w-full p-4 bg-transparent border-white border rounded-lg flex flex-row gap-1 justify-center items-center ${buttonStyles}`}>
+			{isLoading ? (
+				<ActivityIndicator size='small' color='#0E4351' />
+			) : (
+				<Text className={`text-white font-psemibold ${textStyles}`}>
+					{title}
+				</Text>
+			)}
 		</TouchableOpacity>
 	);
 };
